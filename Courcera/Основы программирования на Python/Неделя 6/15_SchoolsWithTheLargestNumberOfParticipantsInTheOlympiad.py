647e2c0c617056a253092d469b5adf026d5d3c8f
GNicoDev <nickholasdvp@gmail.com>
2022-01-14 02:22:31 +0300

    Complete 15 task on 6 week

--- Courcera/Основы программирования на Python/Неделя 6/15_SchoolsWithTheLargestNumberOfParticipantsInTheOlympiad.py
+++ Courcera/Основы программирования на Python/Неделя 6/15_SchoolsWithTheLargestNumberOfParticipantsInTheOlympiad.py
@@ -1,18 +1,38 @@
 inFile = open('input.txt', 'r', encoding='utf-8')
+
+
+class Pupils:
+    def __init__(self, classNum, pupilCount):
+        self.classNum = classNum
+        self.pupilCount = pupilCount
+
+
+def firstOrNone(list, filter):
+    for x in list:
+        if filter(x):
+            return x
+    return None
+
+
 line = inFile.readline()
-numPups = [0] * 99
+pupilsList = []
 while line != '':
     itemsInLine = line.split()
-    numPups[int(itemsInLine[2]) - 1] += 1
+    classNumber = int(itemsInLine[2])
+    a = firstOrNone(pupilsList, lambda x: x.classNum == classNumber)
+    if a is not None:
+        a.pupilCount += 1
+    else:
+        pupilsList.append(Pupils(classNumber, 1))
     line = inFile.readline()
-m = numPups[0]
-scList = list()
-for i in range(1, 99):
-    if numPups[i] > m:
-        scList.clear()
-        scList.append(i + 1)
-        m = numPups[i]
-    elif numPups[i] == m:
-        scList.append(i + 1)
-print(*scList)
+pupilsList.sort(key=lambda k: k.pupilCount, reverse=True)
+resultClasses = [pupilsList[0].classNum]
+maxCount = pupilsList[0].pupilCount
+for i in range(1, len(pupilsList)):
+    if pupilsList[i].pupilCount == maxCount:
+        resultClasses.append(pupilsList[i].classNum)
+    else:
+        break
+resultClasses.sort()
+print(*resultClasses)
 inFile.close()
